5d8855fe4e7e067eb232b86a9af019dca7953451
Kylee Tilley <kyleetilley@gmail.com>
2019-11-03 10:43:59 -0600

    Add max rows to expanding HTML textarea

--- Code.js
+++ Code.js
@@ -1,15 +1,24 @@
-function Code({ get, set, maxRows = 15 }) {
+function Code({ get, set }) {
+  const [maxRows, setMaxRows] = React.useState(15);
   const value = get;
-  const rows = value.split("\n").length;
-  const r = rows > maxRows ? maxRows : rows;
+  const newlines = value.split("\n").length;
+  const rows = newlines > maxRows ? maxRows : newlines;
 
   return React.createElement("div", { className: "Code form-group" }, [
     React.createElement("label", {}, "HTML"),
+
     React.createElement("textarea", {
       className: "form-control",
       onChange: evt => set(evt.target.value),
       value,
-      rows: r
+      rows
+    }),
+    React.createElement("label", {}, "Max Rows"),
+    React.createElement("input", {
+      type: "number",
+      value: maxRows,
+      className: "form-control",
+      onChange: e => setMaxRows(e.target.value)
     })
   ]);
 }
