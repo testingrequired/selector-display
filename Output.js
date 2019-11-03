90bd6e5ca17181f3d650c4b9d26d006466a2fe5d
Kylee Tilley <kyleetilley@gmail.com>
2019-11-03 10:51:04 -0600

    Wrap results in a list of code snippets

--- Output.js
+++ Output.js
@@ -1,3 +1,15 @@
 function Output({ results }) {
-  return React.createElement("pre", { className: "Output" }, results);
+  return React.createElement(
+    "ul",
+    { className: "list-group" },
+    results
+      .split("\n")
+      .map(result =>
+        React.createElement(
+          "li",
+          { className: "list-group-item" },
+          React.createElement("code", {}, result)
+        )
+      )
+  );
 }
