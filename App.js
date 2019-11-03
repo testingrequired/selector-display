be7294a9e79127074407acf7ce77dbbfe7e58f1f
Kylee Tilley <kyleetilley@gmail.com>
2019-11-03 14:41:22 -0600

    Refactor

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 function App() {
   const parser = React.useRef(new DOMParser());
-  const [codeState, setCodeState] = React.useState("");
-  const [selectorState, setSelectorState] = React.useState("");
+  const [html, setHtml] = React.useState("");
+  const [selector, setSelector] = React.useState("");
   const [selectorError, setSelectorError] = React.useState(false);
-  const [queryResults, setQueryResults] = React.useState([]);
+  const [selectorResults, setSelectorResults] = React.useState([]);
 
   React.useEffect(() => {
-    const doc = parser.current.parseFromString(codeState, "text/html");
-
-    if (selectorState) {
+    if (selector) {
       try {
         setSelectorError(false);
-        const elements = doc.querySelectorAll(selectorState);
 
-        setQueryResults(
+        const elements = parser.current
+          .parseFromString(html, "text/html")
+          .querySelectorAll(selector);
+
+        setSelectorResults(
           Array.prototype.slice.call(elements).map(element => element.outerHTML)
         );
       } catch (e) {
@@ -21,19 +22,19 @@ function App() {
           setSelectorError(true);
         }
 
-        setQueryResults([]);
+        setSelectorResults([]);
       }
     } else {
-      setQueryResults([]);
+      setSelectorResults([]);
     }
-  }, [codeState, selectorState]);
+  }, [html, selector]);
 
   const selectorInput = React.createElement(Selector, {
-    get: selectorState,
-    set: setSelectorState
+    get: selector,
+    set: setSelector
   });
 
-  const selectorResults = React.createElement(
+  const selectorErrorAndResults = React.createElement(
     React.Fragment,
     {},
     selectorError &&
@@ -45,19 +46,19 @@ function App() {
     React.createElement("hr"),
     React.createElement("h5", {}, "Results"),
     React.createElement("hr"),
-    React.createElement(Output, { results: queryResults })
+    React.createElement(Output, { results: selectorResults })
   );
 
   return React.createElement(
     "div",
     { className: "App" },
-    React.createElement(Code, { get: codeState, set: setCodeState }),
-    codeState
+    React.createElement(Code, { get: html, set: setHtml }),
+    html
       ? React.createElement(
           React.Fragment,
           {},
           selectorInput,
-          selectorState && selectorResults
+          selector && selectorErrorAndResults
         )
       : React.createElement(
           "p",
