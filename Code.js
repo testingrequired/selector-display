621e682ae88f730f231cf4bc8dd20ac653dc9e86
Kylee Tilley <kyleetilley@gmail.com>
2019-11-04 23:05:31 -0600

    Add invalid HTML warning

--- Code.js
+++ Code.js
@@ -1,32 +1,59 @@
 function Code({ get, set }) {
   const $ = React.createElement;
   const [maxRows, setMaxRows] = React.useState(15);
+  const [invalidHtml, setInvalidHtml] = React.useState(false);
   const value = get;
   const newlines = value.split("\n").length;
   const limitExpand = newlines > maxRows;
   const rows = limitExpand ? maxRows : newlines;
 
   return $(
-    "div",
-    { className: "Code form-group" },
-    $("label", {}, "HTML"),
-    $("textarea", {
-      className: "form-control",
-      onChange: evt => set(evt.target.value),
-      value,
-      rows,
-      style: {
-        resize: "none"
-      }
-    }),
-    limitExpand && $("label", {}, "Max Rows"),
-    limitExpand &&
-      $("input", {
-        type: "number",
-        value: maxRows,
+    React.Fragment,
+    {},
+    $(
+      "div",
+      { className: "Code form-group" },
+      $("label", {}, "HTML"),
+      $("textarea", {
         className: "form-control",
-        min: 1,
-        onChange: e => setMaxRows(e.target.value)
-      })
+        onChange: evt => {
+          set(evt.target.value);
+          if (invalidHtml) setInvalidHtml(false);
+        },
+        onBlur: () => {
+          try {
+            const formatted = prettier.format(value, {
+              parser: "html",
+              plugins: prettierPlugins
+            });
+
+            set(formatted);
+          } catch (e) {
+            console.log(`Invalid HTML: ${e.message}`);
+            setInvalidHtml(true);
+          }
+        },
+        value,
+        rows,
+        style: {
+          resize: "none"
+        }
+      }),
+      limitExpand && $("label", {}, "Max Rows"),
+      limitExpand &&
+        $("input", {
+          type: "number",
+          value: maxRows,
+          className: "form-control",
+          min: 1,
+          onChange: e => setMaxRows(e.target.value)
+        })
+    ),
+    invalidHtml &&
+      $(
+        "p",
+        { className: "alert alert-warning" },
+        "Invalid HTML. Check console for error."
+      )
   );
 }
