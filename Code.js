b12f33d4b3df62c26bf3a01f62f8796ad0f8ed31
Kylee Tilley <kyleetilley@gmail.com>
2019-11-05 00:01:55 -0600

    Autofocus HTML input

--- Code.js
+++ Code.js
@@ -1,16 +1,22 @@
 function Code({ get, set }) {
   const [maxRows, setMaxRows] = React.useState(15);
   const [invalidHtml, setInvalidHtml] = React.useState(false);
+  const textAreaRef = React.useRef(null);
   const value = get;
   const newlines = value.split("\n").length;
   const limitExpand = newlines > maxRows;
   const rows = limitExpand ? maxRows : newlines;
+
+  React.useEffect(() => {
+    textAreaRef.current.focus();
+  }, []);
 
   return html`
     <div>
       <div className="Code form-group">
         <label>HTML</label>
         <textarea
+          ref=${textAreaRef}
           className="form-control"
           onChange=${e => {
             set(e.target.value);
