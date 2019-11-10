347d33c3a426b9d55a83e029f357cef57c203820
Kylee Tilley <kyleetilley@gmail.com>
2019-11-09 20:06:33 -0600

    Add firestore for retrieving shares by id

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 function App() {
   const queryParams = useQuery();
+
   const [htmlInput, setHtmlInput] = React.useState(
     queryParams.get("html") || ""
   );
@@ -9,6 +10,8 @@ function App() {
   const [selectorError, setSelectorError] = React.useState(false);
   const [selectorResults, setSelectorResults] = React.useState([]);
   const parser = React.useRef(new DOMParser());
+
+  const db = React.useRef(firebase.firestore());
 
   function useQuery() {
     return new URLSearchParams(ReactRouterDOM.useLocation().search);
@@ -37,6 +40,28 @@ function App() {
       setSelectorResults([]);
     }
   }, [htmlInput, selector]);
+
+  React.useEffect(() => {
+    if (queryParams.get("share")) {
+      db.current
+        .collection("shares")
+        .doc(queryParams.get("share"))
+        .get()
+        .then(doc => {
+          if (doc.exists) {
+            const data = doc.data();
+
+            if (data.html) {
+              setHtmlInput(data.html);
+            }
+
+            if (data.selector) {
+              setSelector(data.selector);
+            }
+          }
+        });
+    }
+  }, []);
 
   return html`
     <div className="App">
